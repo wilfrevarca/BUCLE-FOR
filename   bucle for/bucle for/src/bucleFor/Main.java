c8cf0b1b07633215bcd9f740950a3383190c2d95
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-07-25 15:34:50 -0500

    Add files via upload

---
+++   bucle for/bucle for/src/bucleFor/Main.java
@@ -0,0 +1,18 @@
+
+ package bucleFor;
+
+    public class Main {
+    public static void main(String[] args) {
+    for(int numero= 0;numero<= 3;numero=numero +1) {
+        System.out.println(numero);
+
+    }
+
+
+
+
+        
+
+
+    }
+    }
